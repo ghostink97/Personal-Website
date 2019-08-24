789bf266647ca4ba4058b5527dee332b2a8dcb79
ourangelofsorrows <angelezrameyer@gmail.com>
2019-08-24 22:24:18 +0200

    mobile version done

--- js/index.js
+++ js/index.js
@@ -17,21 +17,42 @@ $(document).ready(function(){
             console.log('email is valid')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Email is invalid</div>')
+            statusElm.append('<div id="secret-thing">! Email is invalid</div>')
         }
 
         if(subject.length >= 2){
             console.log('subject is valid')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Subject is invalid</div>')
+            statusElm.append('<div id="secret-thing">! Subject must be at least 2 characters long</div>')
         }
 
         if(message.length >= 10){
             console.log('email is valid')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Message is invalid</div>')
+            statusElm.append('<div id="secret-thing">! Message must be at least 10 characters long</div>')
         }
     })
 })
+
+//about section
+
+const aboutBtn=document.querySelector("#about-btn");
+const moreText=document.querySelector("#more-about");
+const lessBtn=document.querySelector("#less-btn");
+
+aboutBtn.addEventListener("click", ShowMore);
+
+function ShowMore(){
+    moreText.classList.toggle("hide");
+    lessBtn.classList.toggle("hide");
+    aboutBtn.classList.toggle("hide");
+}
+
+lessBtn.addEventListener("click", HideInfo);
+function HideInfo(){
+    moreText.classList.toggle("hide");
+    lessBtn.classList.toggle("hide");
+    aboutBtn.classList.toggle("hide");
+}
